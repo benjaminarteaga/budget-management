fee05e0e31ce071dc6b3d37452302a6c00a2df94
Benjamín Arteaga <benjaminjarteagag@gmail.com>
2023-07-16 23:21:48 -0400

    Add Preline

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "node_modules/preline/dist/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [require("rippleui")],
+  plugins: [require("rippleui"), require("preline/plugin")],
 } satisfies Config;
